0ae59ac381e8a35ab3a5b106da116b9916c4b61d
Tarek Rahman <tarekrrr66@gmail.com>
2026-06-16 19:53:00 +0600

    Create labmid.cpp

---
+++ labmid.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main ()
+
+{
+    string str ;
+     getline(cin,str);
+
+    reverse(str.begin(),str.end());
+
+    cout<< "reverse string :" << str <<endl ;
+
+
+
+   return 0;
+
+
+}
